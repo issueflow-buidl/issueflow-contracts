4b5b99d5af90e4cba27d321f521174e2a85463d8
David Ojo <victorojodavid@gmail.com>
2026-04-10 22:03:56 +0100

    feat: scaffold contracts with Hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig } from "hardhat/types";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {},
+  },
+};
+
+export default config;
