0d96bbc22f75f5e2a30fbaea6d93161eeef85da6
Blqd <blqd@users.noreply.github.com>
2026-04-14 09:34:52 +0300

    Fix #6: [CONTRACTS] Implement bounty claim function

--- contracts/bounty/src/lib.rs
+++ contracts/bounty/src/lib.rs
@@ -11,4 +11,10 @@ impl BountyContract {
         let client = token::Client::new(&env, &token);
         client.transfer(&from, &to, &amount);
     }
+
+    pub fn claim_bounty(env: Env, token: Address, maintainer: Address, contributor: Address, amount: i128) {
+        maintainer.require_auth();
+        let client = token::Client::new(&env, &token);
+        client.transfer(&env.current_contract_address(), &contributor, &amount);
+    }
 }
